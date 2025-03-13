06da0960435752c96a58bb723d16d39b98b66fbb
Matheus Lufano <matheussantosmota1910@gmail.com>
2025-03-13 15:29:50 -0300

    Configured work environment

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className='w-full h-full justify-center items-center'>
+      <Text>Configured work environment</Text>
+    </View>
+  );
+}
